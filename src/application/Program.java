97ed693b1430763c2e8d557235e85487f29b4673
allangabr <allanmediogames@gmail.com>
2024-05-25 16:39:01 -0300

    Class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Chess project!");
+		Position pos = new Position(3, 6);
+		System.out.println(pos);
 		
 	}
 
