e010ee02ffe91e03d5cd893b743687087dcfeb88
allangabr <allanmediogames@gmail.com>
2024-06-07 20:06:32 -0300

    Implementação do movimentos possíveis da torre

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
 				UI.ClearScreen();
 				UI.printBoard(chessMatch.getPieces());
 				System.out.println();
-				System.out.print("Source:");
+				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				System.out.println();
 				System.out.print("Target: ");
